6013feedafcf7538d452f2b77dbcf4bdc076a9a2
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2021-12-23 09:39:41 -0800

    Set up skeleton/styling + added logic variables.

---
+++ index.js
@@ -0,0 +1,15 @@
+//game vars
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let playerOneTurn = true;
+
+//DOM vars
+const playerTurnText = document.querySelector("#message");
+const playerOneScoreText = document.querySelector("#player1Scoreboard");
+const playerOneDice = document.querySelector("#player1Dice");
+const playerTwoScoreText = document.querySelector("#player2Scoreboard");
+const playerTwoDice = document.querySelector("#player2Dice");
+const rollButton = document.querySelector("#rollBtn");
+const resetButton = document.querySelector("#resetBtn");
+
+console.log(playerTurnText);
